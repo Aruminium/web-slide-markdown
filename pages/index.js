37f49f3135d7b62685ce316478e19ccffc129ec4
Takagi Kensuke <janus.wel.3@gmail.com>
2021-04-04 16:41:39 +0900

    :heavy_plus_sign::sparkles: implement preview

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import React from "react";
 import Head from "next/head";
 import Editor from "@monaco-editor/react";
+import remark from "remark";
+import parse from "remark-parse";
+import remark2react from "remark-react";
 
 import styles from "../styles/Home.module.css";
 
+const initial = "# Write your own story";
+
 export default function Home() {
+  const [text, setText] = React.useState(initial);
+
+  function handleChange(text) {
+    setText(text);
+  }
+
   return (
     <div>
       <Head>
@@ -14,9 +26,14 @@ export default function Home() {
       <main className={styles.main}>
         <Editor
           height="100vh"
+          width="50%"
           defaultLanguage="markdown"
-          defaultValue="# Write your own story"
+          defaultValue={initial}
+          onChange={handleChange}
         />
+        <div className={styles.preview}>
+          {remark().use(parse).use(remark2react).processSync(text).result}
+        </div>
       </main>
     </div>
   );
