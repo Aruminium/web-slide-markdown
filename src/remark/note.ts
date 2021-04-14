5de98eed6e78a83543e279b2243c9af0e1697eec
Takagi Kensuke <janus.wel.3@gmail.com>
2021-04-14 22:39:31 +0900

    :art: refactor

--- src/remark/note.ts
+++ src/remark/note.ts
@@ -4,10 +4,12 @@ import visit from "unist-util-visit";
 import { Node, Parent } from "unist";
 import { VFileCompatible } from "vfile";
 
+import { isParent, isText } from "./util";
+
 import all from "./all";
 
 function isNoteParentNotation(node: Node) {
-  if (!Array.isArray(node.children)) {
+  if (!isParent(node)) {
     return false;
   }
 
@@ -17,12 +19,13 @@ function isNoteParentNotation(node: Node) {
   }
 
   const firstChild = children[0];
-  if (firstChild.type === "text" && firstChild.value[0] !== "{") {
+  if (!isText(firstChild) || firstChild.value[0] !== "{") {
     return false;
   }
+
   const lastChild = children[children.length - 1];
   if (
-    lastChild.type === "text" &&
+    !isText(lastChild) ||
     lastChild.value[lastChild.value.length - 1] !== "}"
   ) {
     return false;
@@ -32,43 +35,38 @@ function isNoteParentNotation(node: Node) {
 }
 
 export const attacher: unified.Plugin = () => {
-  return (tree: Node, file: VFileCompatible) => {
+  return (tree: Node, _file: VFileCompatible) => {
     visit(tree, "paragraph", visitor);
 
     function visitor(node: Node, index: number, parent: Parent | undefined) {
       if (parent === undefined) {
         return;
       }
-
       if (!isNoteParentNotation(node)) {
         return;
       }
-
-      if (!Array.isArray(node.children)) {
+      if (!isParent(node)) {
         return;
       }
-      const { children } = node;
-      const lastIndex = children.length - 1;
+
+      const lastIndex = node.children.length - 1;
+
+      const children = [...node.children];
+      children[0] = {
+        ...children[0],
+        value: (children[0].value as string).slice(1),
+      };
+      children[lastIndex] = {
+        ...children[lastIndex],
+        value: (children[lastIndex].value as string).slice(
+          0,
+          (children[lastIndex].value as string).length - 1
+        ),
+      };
 
       parent.children[index] = {
         type: "note",
-        children: children.map((child, index) => {
-          if (index !== 0 && index !== lastIndex) {
-            return child;
-          }
-
-          let value = child.value as string;
-          if (index === 0) {
-            value = value.slice(1);
-          }
-          if (index === lastIndex) {
-            value = value.slice(0, value.length - 1);
-          }
-          return {
-            type: "text",
-            value,
-          };
-        }),
+        children,
       };
     }
   };
